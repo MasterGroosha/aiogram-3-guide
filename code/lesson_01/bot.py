a72a6a9190cf15330593bb4de3a677002f7c709e
Evgeny Petrov <groosha@protonmail.com>
2020-08-06 23:29:27 +0300

    Урок №1

---
+++ code/lesson_01/bot.py
@@ -0,0 +1,47 @@
+#!venv/bin/python
+import logging
+from aiogram import Bot, Dispatcher, executor, types
+from os import getenv
+from sys import exit
+
+bot_token = getenv("BOT_TOKEN")
+if not bot_token:
+    exit("Error: no token provided")
+
+bot = Bot(token=bot_token)
+dp = Dispatcher(bot)
+logging.basicConfig(level=logging.INFO)
+
+
+# Хэндлер на команду /test1
+@dp.message_handler(commands="test1")
+async def cmd_test1(message: types.Message):
+    await message.reply("Test 1")
+
+
+# Хэндлер на команду /test2
+async def cmd_test2(message: types.Message):
+    await message.reply("Test 2")
+
+# Где-то в другом месте...
+dp.register_message_handler(cmd_test2, commands="test2")
+
+
+@dp.message_handler(commands="answer")
+async def cmd_answer(message: types.Message):
+    await message.answer("Это простой ответ")
+
+
+@dp.message_handler(commands="reply")
+async def cmd_reply(message: types.Message):
+    await message.reply('Это ответ с "ответом"')
+
+
+@dp.message_handler(commands="dice")
+async def cmd_dice(message: types.Message):
+    await message.answer_dice(emoji="🎲")
+
+
+if __name__ == "__main__":
+    # Запускаем бота и пропускаем все накопленые входящие
+    executor.start_polling(dp, skip_updates=True)
