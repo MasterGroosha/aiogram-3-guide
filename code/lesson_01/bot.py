6fef85d96a47a961f6750582302635b60bcefeb7
Evgeny Petrov <groosha@protonmail.com>
2020-09-05 12:50:49 +0300

    Добавлен код про обработку ошибок

--- code/lesson_01/bot.py
+++ code/lesson_01/bot.py
@@ -1,6 +1,8 @@
 #!venv/bin/python
 import logging
+import asyncio
 from aiogram import Bot, Dispatcher, executor, types
+from aiogram.utils.exceptions import BotBlocked
 from os import getenv
 from sys import exit
 
@@ -40,6 +42,23 @@ async def cmd_reply(message: types.Message):
 @dp.message_handler(commands="dice")
 async def cmd_dice(message: types.Message):
     await message.answer_dice(emoji="🎲")
+
+
+@dp.message_handler(commands="block")
+async def cmd_block(message: types.Message):
+    await asyncio.sleep(10.0)  # Здоровый сон на 10 секунд
+    await message.reply("Вы заблокированы")
+
+
+@dp.errors_handler(exception=BotBlocked)
+async def error_bot_blocked(update: types.Update, exception: BotBlocked):
+    # Update: объект события от Telegram. Exception: объект исключения
+    # Здесь можно как-то обработать блокировку, например, удалить пользователя из БД
+    print(f"Меня заблокировал пользователь!\nСообщение: {update}\nОшибка: {exception}")
+
+    # Такой хэндлер должен всегда возвращать True,
+    # если дальнейшая обработка не требуется.
+    return True
 
 
 if __name__ == "__main__":
