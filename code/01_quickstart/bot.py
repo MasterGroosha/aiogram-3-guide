defc82dd32a15304f55a143b8f1f070ab39def7c
Groosha <groosha@protonmail.com>
2022-03-27 01:06:17 +0300

    Обновлена первая глава

--- code/01_quickstart/bot.py
+++ code/01_quickstart/bot.py
@@ -1,66 +1,55 @@
-#!venv/bin/python
-import logging
 import asyncio
-from aiogram import Bot, Dispatcher, executor, types
-from aiogram.utils.exceptions import BotBlocked
-from os import getenv
-from sys import exit
+import logging
 
-bot_token = getenv("BOT_TOKEN")
-if not bot_token:
-    exit("Error: no token provided")
+from aiogram import Bot, Dispatcher, types
 
-bot = Bot(token=bot_token)
-dp = Dispatcher(bot)
 logging.basicConfig(level=logging.INFO)
+bot = Bot(token="TOKEN")
+dp = Dispatcher()
+
+
+# Хэндлер на команду /start
+@dp.message(commands="start")
+async def cmd_start(message: types.Message):
+    await message.answer("Hello!")
 
 
 # Хэндлер на команду /test1
-@dp.message_handler(commands="test1")
+@dp.message(commands="test1")
 async def cmd_test1(message: types.Message):
-    await message.reply("Test 1")
+    await message.answer("Test 1")
 
 
 # Хэндлер на команду /test2
+# Без декоратора, т.к. регистрируется ниже в функции main()
 async def cmd_test2(message: types.Message):
     await message.reply("Test 2")
 
-# Где-то в другом месте...
-dp.register_message_handler(cmd_test2, commands="test2")
 
-
-@dp.message_handler(commands="answer")
+@dp.message(commands="answer")
 async def cmd_answer(message: types.Message):
     await message.answer("Это простой ответ")
 
 
-@dp.message_handler(commands="reply")
+@dp.message(commands="reply")
 async def cmd_reply(message: types.Message):
     await message.reply('Это ответ с "ответом"')
 
 
-@dp.message_handler(commands="dice")
+@dp.message(commands="dice")
 async def cmd_dice(message: types.Message):
     await message.answer_dice(emoji="🎲")
 
 
-@dp.message_handler(commands="block")
-async def cmd_block(message: types.Message):
-    await asyncio.sleep(10.0)  # Здоровый сон на 10 секунд
-    await message.reply("Вы заблокированы")
+async def main():
+    # Регистрируем хэндлер cmd_test2 по команде /start
+    dp.message.register(cmd_test2, commands="test2")
 
-
-@dp.errors_handler(exception=BotBlocked)
-async def error_bot_blocked(update: types.Update, exception: BotBlocked):
-    # Update: объект события от Telegram. Exception: объект исключения
-    # Здесь можно как-то обработать блокировку, например, удалить пользователя из БД
-    print(f"Меня заблокировал пользователь!\nСообщение: {update}\nОшибка: {exception}")
-
-    # Такой хэндлер должен всегда возвращать True,
-    # если дальнейшая обработка не требуется.
-    return True
+    # Запускаем бота и пропускаем все накопленные входящие
+    # Да, этот метод можно вызвать даже если у вас поллинг
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot)
 
 
 if __name__ == "__main__":
-    # Запускаем бота и пропускаем все накопленые входящие
-    executor.start_polling(dp, skip_updates=True)
+    asyncio.run(main())
---
+++ code/01_quickstart/run.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+exec python bot.py
