8504b65a1d77799ee6953676a87109cae89c4df7
artloop <54043473+artloop@users.noreply.github.com>
2023-04-20 12:22:18 +0300

    Пример загрузки файлов (#33)
    Расширен текст главы "Работа с сообщениями", добавлен целый кусок текста про загрузку (upload) файлов в Telegram
    Спасибо artloop (https://github.com/artloop) за помощь!
    ---------
    
    Co-authored-by: Groosha <groosha@protonmail.com>

--- code/02_messages/bot.py
+++ code/02_messages/bot.py
@@ -4,6 +4,7 @@ from datetime import datetime
 
 from aiogram import Bot, Dispatcher, types, html, F
 from aiogram.filters import Command, CommandObject
+from aiogram.types import FSInputFile, URLInputFile, BufferedInputFile
 from aiogram.utils.markdown import hide_link
 
 from config_reader import config
@@ -37,6 +38,43 @@ async def cmd_hidden_link(message: types.Message):
         f"Пользователи: *не читают документацию*\n"
         f"Груша:"
     )
+
+
+@dp.message(Command('images'))
+async def upload_photo(message: types.Message):
+    # Сюда будем помещать file_id отправленных файлов, чтобы потом ими воспользоваться
+    file_ids = []
+
+    # Чтобы продемонстрировать BufferedInputFile, воспользуемся "классическим"
+    # открытием файла через `open()`. Но, вообще говоря, этот способ
+    # лучше всего подходит для отправки байтов из оперативной памяти
+    # после проведения каких-либо манипуляций, например, редактированием через Pillow
+    with open("buffer_emulation.jpg", "rb") as image_from_buffer:
+        result = await message.answer_photo(
+            BufferedInputFile(
+                image_from_buffer.read(),
+                filename="image from buffer.jpg"
+            ),
+            caption="Изображение из буфера"
+        )
+        file_ids.append(result.photo[-1].file_id)
+
+    # Отправка файла из файловой системы
+    image_from_pc = FSInputFile("image_from_pc.jpg")
+    result = await message.answer_photo(
+        image_from_pc,
+        caption="Изображение из файла на компьютере"
+    )
+    file_ids.append(result.photo[-1].file_id)
+
+    # Отправка файла по ссылке
+    image_from_url = URLInputFile("https://picsum.photos/seed/groosha/400/300")
+    result = await message.answer_photo(
+        image_from_url,
+        caption="Изображение по ссылке"
+    )
+    file_ids.append(result.photo[-1].file_id)
+    await message.answer("Отправленные файлы:\n"+"\n".join(file_ids))
 
 
 @dp.message(F.text)
