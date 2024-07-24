b484e60d700d90f8b193b554ffb676012bc643d1
Rav <107697105+Ravvw@users.noreply.github.com>
2024-07-24 17:10:59 +0300

    Update chat_type.py

--- code/05_filters_and_middlewares/filters/chat_type.py
+++ code/05_filters_and_middlewares/filters/chat_type.py
@@ -5,11 +5,8 @@ from aiogram.types import Message
 
 
 class ChatTypeFilter(BaseFilter):
-    def __init__(self, chat_type: Union[str, list]):
-        self.chat_type = chat_type
+    def __init__(self, is_private: bool):
+        self.is_private = is_private
 
     async def __call__(self, message: Message) -> bool:
-        if isinstance(self.chat_type, str):
-            return message.chat.type == self.chat_type
-        else:
-            return message.chat.type in self.chat_type
+        return self.is_private == (message.chat.type == 'private')
